5bb8f686afd38a44ac29a5600d5369aa442a67d2
Jakub Pajor <jay.pajor@gmail.com>
2018-04-12 00:12:43 +0200

    small fix

--- Sysopy/Zestaw4/Zad3/zad3parent.c
+++ Sysopy/Zestaw4/Zad3/zad3parent.c
@@ -12,9 +12,9 @@
 
 volatile int L = 0;
 volatile int TYPE = 0;
-volatile int sentToChild = 0;
-volatile int receivedByChild = 0;
-volatile int receivedFromChild = 0;
+volatile int sigs_sent = 0;
+volatile int child_sigs_received = 0;
+volatile int parent_sigs_received = 0;
 volatile pid_t child;
 
 
@@ -25,77 +25,77 @@ void signals_handler(int signum, siginfo_t *info, void *context) {
         kill(child, SIGUSR2);
         exit(EXIT_FAILURE);
     }
-
+    
     if ((signum == SIGUSR1 || signum == SIGRTMIN) && info->si_pid == child) {
-        receivedFromChild++;
+        parent_sigs_received++;
         printf("Parent: Received signal: %d form Child\n",signum);
     }
 }
 
 void parent_is_coming() {
     sleep(1);
-
+    
     struct sigaction act;
     sigemptyset(&act.sa_mask);
     act.sa_flags = SA_SIGINFO;
     act.sa_sigaction = signals_handler;
-
+    
     if (sigaction(SIGINT, &act, NULL) == -1)
         fprintf(stderr,"Can't catch SIGINT\n");
-
+    
     if (TYPE == 1 || TYPE == 2)
         if (sigaction(SIGUSR1, &act, NULL) == -1) fprintf(stderr,"Can't catch SIGUSR1\n");
     if (TYPE == 3)
         if (sigaction(SIGRTMIN, &act, NULL) == -1) fprintf(stderr,"Can't catch SIGRTMIN\n");
-
+    
     if (TYPE == 1 || TYPE == 2) {
         sigset_t mask;
         sigfillset(&mask);
         sigdelset(&mask, SIGUSR1);
         sigdelset(&mask, SIGINT);
-        while(sentToChild < L){
+        while(sigs_sent < L){
             printf("Parent: Sending SIGUSR1\n");
             kill(child, SIGUSR1);
             if (TYPE == 2) sigsuspend(&mask);
-	    sentToChild++;
+            sigs_sent++;
         }
         printf("Parent: Sending SIGUSR2\n");
         kill(child, SIGUSR2);
     }
     if (TYPE == 3) {
-        while(sentToChild < L){
+        while(sigs_sent < L){
             printf("Parent: Sending SIGRTMIN\n");
             kill(child, SIGRTMIN);
-	    sentToChild++;
+            sigs_sent++;
         }
-        sentToChild++;
+        sigs_sent++;
         printf("Parent: Sending SIGRTMAX\n");
         kill(child, SIGRTMAX);
     }
-
+    
     int status = 0;
     waitpid(child, &status, 0);
-    if (WIFEXITED(status))
-        receivedByChild = WEXITSTATUS(status);
+    if (WIFEXITED(status)) child_sigs_received = WEXITSTATUS(status);
     else fprintf(stderr,"Sth went wrong with termination of Child!\n");
 }
 
 
 int main(int argc, char *argv[]) {
-
+    
     if (argc < 3) {fprintf(stderr,"Please give arguments: first - signals numbers, second - mode.\n"); exit(EXIT_FAILURE); }
     L = atoi(argv[1]);
     if(L < 1) {fprintf(stderr,"Wrrrr... Given sig numbers is wrong.\n"); exit(EXIT_FAILURE);}
-
+    
     TYPE = atoi(argv[2]);
     if (TYPE < 1 || TYPE > 3) {fprintf(stderr,"Wrrrr... Mode argument should be 1, 2 or 3.\n"); exit(EXIT_FAILURE);}
     
     if ((child = fork()) == 0) {execl("./zad3child","./zad3child",argv[2]); exit(EXIT_FAILURE);}
     else if (child > 0) parent_is_coming();
     else fprintf(stderr,"Forking went wrong... .\n");
-
-    printf("\nParent: signals sent: %d.\n", sentToChild);
-    printf("Parent: signals received from child: %d.\n", receivedFromChild);
-
+    
+    printf("\nParent: signals sent: %d.\n", sigs_sent);
+    printf("Parent: signals received from child: %d.\n", parent_sigs_received);
+    
     return 0;
 }
+
