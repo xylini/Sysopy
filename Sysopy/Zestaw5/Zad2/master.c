d1ecece21ed5170040b4c6612ab096fdb43ba594
Jakub Pajor <jay.pajor@gmail.com>
2018-04-18 22:03:00 +0200

    comment fix

--- Sysopy/Zestaw5/Zad2/master.c
+++ Sysopy/Zestaw5/Zad2/master.c
@@ -12,7 +12,7 @@ int main(int argc, char * argv[]){
     mkfifo(argv[1], S_IWUSR | S_IRUSR);
     char buffer[512];
     FILE * fifopipo;
-    //printf("Master started, PID - %d.\n",getpid());
+    printf("Master started, PID - %d.\n",getpid());
     if((fifopipo = fopen(argv[1], "r")) == NULL){ printf("Error while opening FIFO.\n"); exit(EXIT_FAILURE);}
     while(fgets(buffer, 512, fifopipo) != NULL){ write(1, buffer, strlen(buffer));}
     fclose(fifopipo);
